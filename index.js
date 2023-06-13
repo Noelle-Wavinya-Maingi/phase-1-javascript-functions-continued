618361e45c0b03917209c0978f0201c8410f7306
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-13 23:15:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(defaultActivity = "roller-skate") {
+  return `This Saturday, I want to ${defaultActivity}!`;
+}
+console.log(saturdayFun());
+
+function mondayWork(defaultActivity = "go to the office") {
+  return `This Monday, I will ${defaultActivity}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(sign) {
+  return function (Adjective) {
+    return `You are ${sign}${Adjective}${sign}!`;
+  };
+}
